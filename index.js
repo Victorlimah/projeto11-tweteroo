497497fd0aef668a3d500279fc926b24d1217d5b
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-04-20 22:58:57 -0300

    refact code: function paramsIsInvalid and post /tweets

--- index.js
+++ index.js
@@ -6,42 +6,45 @@ app.use(json());
 app.use(cors());
 
 let users = [];
-let tweets = [
-  {
-    username: "bobesponja",
-    avatar:
-      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub",
-  },
-];
+let tweets = [];
 
 app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
 
-  if (paramsIsInvalid()) {
+  if (paramsIsInvalid(username, avatar)) {
     res.status(400).send("Todos os campos são obrigatórios!");
   } else {
     users.push(req.body);
-    res.status(200).send("OK");
-  }
-
-  function paramsIsInvalid() {
-    if (username === undefined || username === "") return true;
-    if (avatar === undefined || avatar === "") return true;
-    if (!avatar.startsWith("http")) return true;
-    return false;
+    res.status(201).send("OK");
   }
 });
 
 app.get("/tweets", (req, res) => {
-  res.status(200).send(tweets);
+  // pegar apenas os 10 ultimos tweets
+  const newTweets = [...tweets.reverse()];
+  res.status(201).send(newTweets.slice(0, 10));
 });
 
 app.post("/tweets", (req, res) => {
-  const { username, tweet } = req.body;
-  const avatar = users.find((user) => user.username === username).avatar;
-  tweets.push({ username, tweet, avatar });
+  // depois fazer paginacao
+  const { tweet } = req.body;
+  const { user } = req.headers;
+  console.log(user, tweet);
+  if (paramsIsInvalid(tweet)) {
+    res.status(400).send("Todos os campos são obrigatórios!");
+  } else {
+    const avatar = users.find((u) => u.username === user).avatar;
+    tweets.push({ username: user, tweet, avatar });
+    res.status(201).send("OK");
+  }
 });
+
+function paramsIsInvalid(username = null, avatar = null, tweet = null) {
+  if (username === undefined || username === "") return true;
+  if (avatar === undefined || avatar === "") return true;
+  if (tweet === undefined || tweet === "") return true;
+  return false;
+}
 
 app.listen(5000, () => {
   console.log("Servidor iniciado em http://localhost:5000");
